0a24835f4d6770f2ec1fba918c1a77c918c19236
alanssmeira <alan.sobreirasa@gmail.com>
2022-05-19 16:29:40 -0300

    Initial commit

---
+++ fabricantes/atualizar.php
@@ -0,0 +1,49 @@
+<?php
+require '../includes/funcoes-fabricantes.php';
+
+// Capturar o parâmetro id da URL
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+//Chamamos a função que irá retornar os dado de UM FABRICANTE
+$dados = lerUmFabricante($conexao, $id);
+
+// Detectando o acionamento da botão atualizar
+if (isset($_POST['atualizar'])) {
+    $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+    atualizarFabricante($conexao, $id, $nome);
+    header("location:listar.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title> Fabricantes | UPDATE - CRUD com PHP e MySQL </title>
+<link href="../css/style.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <h1>Fabricantes | SELECT e UPDATE -
+    <a href="listar.php">Listar</a> - 
+    <a href="../index.php">Home</a> </h1>
+</div>
+
+<div class="container">
+
+    <h2>Utilize o formulário abaixo para atualizar os dados de um fabricante.</h2>
+
+    <form action="" method="post" class="w-50 mx-auto">
+
+	    <p>
+            <label for="nome">Nome:</label><br>
+	        <input value="<?=$dados['nome']?>" type="text" name="nome" id="nome" required>
+        </p>   
+        <button name="atualizar">Atualizar fabricante</button>
+	</form>	
+
+</div>
+
+</body>
+</html>
---
+++ fabricantes/listar.php
@@ -0,0 +1,61 @@
+<?php
+require "../includes/funcoes-fabricantes.php";
+$listaDeFabricantes = lerFabricantes($conexao);
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title> Fabricantes | SELECT - CRUD com PHP e MySQL </title>
+<link href="../css/style.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+    <h1>Fabricantes | SELECT -
+    <a href="../index.php">Home</a> </h1>
+</div>
+      
+
+<div class="container">
+    
+    <h2>Lendo e carregando todos os fabricantes</h2>
+    <p><a href="inserir.php">Inserir</a></p>    
+
+    <table>
+        <caption> Lista de Fabricantes </caption>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Operação</th>
+            </tr>
+        </thead>
+                
+        <tbody>
+
+<?php foreach( $listaDeFabricantes as $fabricante ){ ?>        
+            <tr>
+                <td> <?=$fabricante["id"]?> </td>
+                <td> <?=$fabricante["nome"]?> </td>
+                <td> 
+                    <a href="atualizar.php?id=<?=$fabricante["id"]?>">Atualizar</a> 
+                    - <a href="excluir.php?id=<?=$fabricante["id"]?>">Excluir</a>
+                </td>
+            </tr> 
+<?php 
+} 
+require "../includes/desconecta.php"; // opcional
+?>
+
+        </tbody>
+
+    </table>
+ 
+</div>
+
+
+
+</body>
+</html>
