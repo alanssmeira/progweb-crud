e1dfc77bb896051709a8cd37200af46a7fd5e2cf
alanssmeira <alan.sobreirasa@gmail.com>
2022-05-24 15:52:19 -0300

    Update listar.php

--- produtos/listar.php
+++ produtos/listar.php
@@ -4,37 +4,46 @@ $listaDeProdutos = lerProdutos($conexao);
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Produtos | SELECT - CRUD com PHP e MySQL </title>
-<link href="../css/style.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Produtos | SELECT - CRUD com PHP e MySQL </title>
+    <link href="../css/style.css" rel="stylesheet">
 </head>
+
 <body>
 
-<div class="container">
-    <h1>Produtos | SELECT -
-    <a href="../index.php">Home</a> </h1>
-</div>
+    <div class="container">
+        <h1>Produtos | SELECT -
+            <a href="../index.php">Home</a>
+        </h1>
+    </div>
 
-<div class="container">
-    
-    <h2>Lendo e carregando todos os produtos</h2>
-    <p><a href="inserir.php">Inserir</a></p>  
+    <div class="container">
 
-    <hr>
+        <h2>Lendo e carregando todos os produtos</h2>
+        <p><a href="inserir.php">Inserir</a></p>
 
+        <hr>
 
-    <ul>
-        <li><b>Nome:</b>  </li>
-        <li><b>Preço:</b>  </li>
-        <li><b>Quantidade:</b>  </li>
-        <li><b>Descrição:</b>   </li>
-        <li><b>Fabricante:</b>  </li>
-    </ul>
+        <?php foreach ($listaDeProdutos as $produto) { ?>
+            <ul>
+                <li><b>Nome:</b> <?= $produto['produto'] ?> </li>
+                <li><b>Preço:</b> <?= $produto['preco'] ?> </li>
+                <li><b>Quantidade:</b> <?= $produto['quantidade'] ?> </li>
+                <li><b>Descrição:</b> <?= $produto['descricao'] ?> </li>
+                <li><b>Fabricante:</b> <?= $produto['fabricante'] ?> </li>
+            </ul>
 
-</div>
+            <a href="atualizar.php?id=<?=$produto['id']?>">Atualizar</a>
+            <a href="excluir.php?id=<?=$produto['id']?>">Excluir</a>
+
+        <?php } ?>
+
+    </div>
 
 
 </body>
+
 </html>
