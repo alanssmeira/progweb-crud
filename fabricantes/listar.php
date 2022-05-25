2fa60cce1dbc243b13189573755adab53d1cfadf
alanssmeira <alan.sobreirasa@gmail.com>
2022-05-25 16:39:36 -0300

    bootstrap 4.6

--- fabricantes/listar.php
+++ fabricantes/listar.php
@@ -4,58 +4,85 @@ $listaDeFabricantes = lerFabricantes($conexao);
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title> Fabricantes | SELECT - CRUD com PHP e MySQL </title>
-<link href="../css/style.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Fabricantes | SELECT - CRUD com PHP e MySQL </title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="../css/style.css"> -->
+
 </head>
+
 <body>
 
-<div class="container">
-    <h1>Fabricantes | SELECT -
-    <a href="../index.php">Home</a> </h1>
-</div>
-      
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="../index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="listar.php">Fabricantes</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="../produtos/listar.php">Produtos</a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
-<div class="container">
-    
-    <h2>Lendo e carregando todos os fabricantes</h2>
-    <p><a href="inserir.php">Inserir</a></p>    
-
-    <table>
-        <caption> Lista de Fabricantes </caption>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>Operação</th>
-            </tr>
-        </thead>
-                
-        <tbody>
-
-<?php foreach( $listaDeFabricantes as $fabricante ){ ?>        
-            <tr>
-                <td> <?=$fabricante["id"]?> </td>
-                <td> <?=$fabricante["nome"]?> </td>
-                <td> 
-                    <a href="atualizar.php?id=<?=$fabricante["id"]?>">Atualizar</a> 
-                    - <a href="excluir.php?id=<?=$fabricante["id"]?>">Excluir</a>
-                </td>
-            </tr> 
-<?php 
-} 
-require "../includes/desconecta.php"; // opcional
-?>
-
-        </tbody>
-
-    </table>
- 
-</div>
+    <div class="container text-center mt-3">
+        <h1>Fabricantes | SELECT -
+            <a class="btn btn-dark" href="../index.php">Home</a>
+        </h1>
+    </div>
 
 
+    <div class="container text-center">
 
+        <h2>Lendo e carregando todos os fabricantes</h2>
+        <p><a class="btn btn-primary btn-lg" href="inserir.php">Inserir</a></p>
+
+        <table class="table table-striped">
+            <caption> Lista de Fabricantes </caption>
+            <thead class="thead-dark">
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Operação</th>
+                </tr>
+            </thead>
+
+            <tbody>
+
+                <?php foreach ($listaDeFabricantes as $fabricante) { ?>
+                    <tr>
+                        <td> <?= $fabricante["id"] ?> </td>
+                        <td> <?= $fabricante["nome"] ?> </td>
+                        <td>
+                            <a class="btn btn-warning" href="atualizar.php?id=<?= $fabricante["id"] ?>">Atualizar</a>
+                            - <a class=" btn btn-danger" href="excluir.php?id=<?= $fabricante["id"] ?>">Excluir</a>
+                        </td>
+                    </tr>
+                <?php
+                }
+                require "../includes/desconecta.php"; // opcional
+                ?>
+
+            </tbody>
+
+        </table>
+
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- produtos/listar.php
+++ produtos/listar.php
@@ -9,12 +9,13 @@ $listaDeProdutos = lerProdutos($conexao);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Produtos | SELECT - CRUD com PHP e MySQL </title>
-    <link href="../css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <!-- <link href="../css/style.css" rel="stylesheet"> -->
 </head>
 
 <body>
 
-    <div class="container">
+    <div class="container text-center mt-3">
         <h1>Produtos | SELECT -
             <a href="../index.php">Home</a>
         </h1>
@@ -23,27 +24,32 @@ $listaDeProdutos = lerProdutos($conexao);
     <div class="container">
 
         <h2>Lendo e carregando todos os produtos</h2>
-        <p><a href="inserir.php">Inserir</a></p>
+        <p><a class="btn btn-primary btn-lg" href="inserir.php">Inserir</a></p>
 
         <hr>
 
-        <?php foreach ($listaDeProdutos as $produto) { ?>
-            <ul>
-                <li><b>Nome:</b> <?= $produto['produto'] ?> </li>
-                <li><b>Preço:</b> <?= formataMoeda($produto['preco']) ?> </li>
-                <li><b>Quantidade:</b> <?= $produto['quantidade'] ?> </li>
-                <li><b>Descrição:</b> <?= $produto['descricao'] ?> </li>
-                <li><b>Fabricante:</b> <?= $produto['fabricante'] ?> </li>
-            </ul>
-
-            <a href="atualizar.php?id=<?=$produto['id']?>">Atualizar</a>
-            <a href="excluir.php?id=<?=$produto['id']?>">Excluir</a>
-
-        <?php } ?>
+        <div class="row">
+            <?php foreach ($listaDeProdutos as $produto) { ?>
+                <article class="col-sm-6 col-lg-4">
+                    <h3> <?= $produto['produto'] ?> </h3>
+                    <p><b>Preço:</b> <?= formataMoeda($produto['preco']) ?> </p>
+                    <p><b>Quantidade:</b> <?= $produto['quantidade'] ?> </p>
+                    <p><b>Descrição:</b> <?= $produto['descricao'] ?> </p>
+                    <p><b>Fabricante:</b> <?= $produto['fabricante'] ?> </p>
+                
+            
+                <p>
+                    <a href="atualizar.php?id=<?= $produto['id'] ?>">Atualizar</a>
+                    <a href="excluir.php?id=<?= $produto['id'] ?>">Excluir</a>
+                </p>
+                </article>
+            <?php } ?>
+        </div>
 
     </div>
 
-
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 </body>
 
 </html>
